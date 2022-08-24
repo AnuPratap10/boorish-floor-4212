20c82b08eef8a46bf42330e4cff19babee4df993
AnuPratap10 <anupratapsingh49@gmail.com>
2022-08-25 03:34:46 +0530

    day-2 nav

---
+++ src/Component/Body.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import {MyNavbar} from "./MyNavbar"
+
+export const Body = () => {
+  return (
+    <>
+  <div className="mainBody">
+  <div className="bodyNav">
+
+    <MyNavbar/>
+    </div>
+  </div>
+  </>
+  )
+}
