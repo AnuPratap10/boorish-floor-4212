be2ebf2b7448978036e22183e347615d3003de5c
AnuPratap10 <anupratapsingh49@gmail.com>
2022-08-26 03:31:46 +0530

    day-3 gray page

--- src/Component/Body.jsx
+++ src/Component/Body.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { GrayPage } from './GrayPage'
 
 import {MyNavbar} from "./MyNavbar"
+import { SecPart } from './SecPart'
 
 export const Body = () => {
   return (
     <>
   <div className="mainBody">
-  <div className="bodyNav">
-
-    <MyNavbar/>
-    </div>
+  <div className="bodyNav"> <MyNavbar/> </div>
+   <div  className="secPart">    <SecPart/></div>
+    <div className="grayPage" > <GrayPage/>  </div>
   </div>
   </>
   )
